4cdb681e67bbb46f373d437c98d70027727fbdf5
Teslenko A.A.(work) <alteslo31@gmail.com>
2022-04-08 17:53:53 +0300

    start create Custom user (AbstarctBaseUser)

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+{
+  "first_name": "string",
+  "last_name": "string",
+  "other_name": "string",
+  "email": "string",
+  "phone": "string",
+  "birthday": "2022-04-08",
+  "city": 0,
+  "additional_info": "string",
+  "is_admin": true,
+  "password": "string"
+}
